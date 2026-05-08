7eb1e0592d52875bc6fb6ace1b0e1adcbe1ba005
Ethanerz <ethangapulan@gmail.com>
2026-05-08 12:40:42 +0800

    added signup and signin buttons to laning page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
-
-function App(){
-  return
-	<>
-    <Signup />
-  </>;
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to Our App</h1>
+      <div className="button-container">
+        <Link to="/signup">
+          <button className="signup-btn">Sign Up</button>
+        </Link>
+        <br />
+        <Link to="/signin">
+          <button className="signin-btn">Sign In</button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
